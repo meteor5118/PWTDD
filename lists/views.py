aa63b0054e38863f7fe40d8b3fd1cbd73143586d
chenglong.zhang <chenglong.zhang@qingteng.me>
2015-12-07 17:18:58 +0800

    Model for list Items and associated migration

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpRequest
+import logging
+
+logger = logging.getLogger(__name__)
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
